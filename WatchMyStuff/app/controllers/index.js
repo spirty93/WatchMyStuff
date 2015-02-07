2b14ab4354cad4642ae4a0ea0f0177a9363d8710
spirty <spirty93@gmail.com>
2015-02-07 14:04:43 -0500

    Some stuff working

--- WatchMyStuff/app/controllers/index.js
+++ WatchMyStuff/app/controllers/index.js
@@ -1,7 +1,22 @@
 $.index.open();
 
+function ShowDialog(){
+	$.dialog.show();
+}
+
+function StoreName(){
+	alert("Stored");	
+}
+
 function doClick (e) {
-	 var view = Alloy.createController("monitorWatch", {}).getView();
-	 Titanium.API.info(view);
-   	 view.open();
-};
+	var view = Alloy.createController("WatchComplete", {}).getView();
+	Titanium.API.info(view);
+   	view.open();
+}
+
+$.index.addEventListener('open', function(){
+	if (!Ti.App.LogInName){
+		var view = Alloy.createController("Login", {}).getView();
+		view.open();
+	}
+});
